c38ac7ff208ea01767cfe627eb33fa25804b7cfa
Wei Dong <wdong@wdong.org>
2018-06-11 20:48:10 -0400

    update

--- curb18a/train.sh
+++ curb18a/train.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-MODEL=resnet_v2_18_slim
-#MODEL=resnet_v2_18     # reduce --batch if OOM
+#MODEL=resnet_v2_18_slim
+MODEL=resnet_v2_18     # reduce --batch if OOM
 #MODEL=resnet_v2_50
 
+reduction=4            # reduce channels by this factor before deconv
+                       # resnet has 512 channels and will reduce like (512 ->) 128 -> 64 -> 32 -> 16
 ../train-fcn-slim.py \
                      --db train.db \
                      --val_db val.db \
                      --backbone $MODEL \
                      --backbone_stride 16 \
-                     --reduction 2 \            # reduce channels by this factor before deconvolution
+                     --reduction $reduction \
                      --batch 8 \
                      --fix_width 640 \
                      --fix_height 400 \
@@ -20,6 +22,9 @@ MODEL=resnet_v2_18_slim
                      --noadam \
                      --weight_decay 2.5e-4 \
                      --dice \
-                     --model $MODEL \           # save to this path
+                     --model $MODEL \
                      $*
 
+# predict
+# ../predict-fcn.py --max_size 640 --list val.txt --model resnet_v2_18/120
+# ../predict-fcn.py --max_size 640 --db val.db --model resnet_v2_18/120
--- predict-fcn.py
+++ predict-fcn.py
@@ -5,7 +5,8 @@ import numpy as np
 import cv2
 import tensorflow as tf
 from tensorflow.python.framework import meta_graph
-from gallery import gallery
+import picpac
+from gallery import Gallery
 
 class Model:
     def __init__ (self, X, path, name):
@@ -24,8 +25,15 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('model', None, '')
-flags.DEFINE_integer('stride', 16, '')
+flags.DEFINE_integer('clip_stride', 16, '')
 flags.DEFINE_integer('max_size', 2000, '')
+flags.DEFINE_integer('channels', 3, '')
+flags.DEFINE_string('colorspace', 'RGB', '')
+
+flags.DEFINE_string('db', None, '')
+flags.DEFINE_string('list', None, '')
+flags.DEFINE_integer('max', 50, '')
+
 
 def save_prediction_image (gal, image, prob):
     cv2.imwrite(gal.next(), image)
@@ -46,32 +54,58 @@ def main (_):
         sess.run(tf.local_variables_initializer())
         model.loader(sess)
 
-        gal = gallery.Gallery('output', cols=2, ext='.jpg')
-        with open('val.txt', 'r') as f:
-            for path in f:
-                path = path.strip()
-                print(path)
-                image = cv2.imread(path, -1)
-                H, W = image.shape[:2]
-
-                if max(H, W) > FLAGS.max_size:
-                    f = FLAGS.max_size / max(H, W)
-                    image = cv2.resize(image, None, fx=f, fy=f)
+        gal = Gallery('output', cols=2, ext='.jpg')
+        CC = 0
+        if FLAGS.list:
+            with open(FLAGS.list, 'r') as f:
+                for path in f:
+                    if CC > FLAGS.max:
+                        break
+                    path = path.strip()
+                    print(path)
+                    if FLAGS.channels == 3:
+                        image = cv2.imread(path, cv2.IMREAD_COLOR)
+                    elif FLAGS.channels == 1:
+                        image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                    else:
+                        assert False
                     H, W = image.shape[:2]
-                '''BEGIN INFERENCE'''
-                # clip edge
-                H = H // FLAGS.stride * FLAGS.stride
-                W = W // FLAGS.stride * FLAGS.stride
-                image = image[:H, :W, :].astype(np.float32)
-                # change from BGR to RGB
-                image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-                batch = np.expand_dims(image_rgb, axis=0)
-                prob = sess.run(model.prob, feed_dict={X: batch})
-                '''END INFERENCE'''
-                save_prediction_image(gal, image, prob[0])
-            gal.flush()
-        pass
+                    if max(H, W) > FLAGS.max_size:
+                        f = FLAGS.max_size / max(H, W)
+                        image = cv2.resize(image, None, fx=f, fy=f)
+                        H, W = image.shape[:2]
+                    '''BEGIN INFERENCE'''
+                    # clip edge
+                    H = H // FLAGS.clip_stride * FLAGS.clip_stride
+                    W = W // FLAGS.clip_stride * FLAGS.clip_stride
+                    image = image[:H, :W].astype(np.float32)
+                    # change from BGR to RGB
+                    if FLAGS.channels == 3 and FLAGS.colorspace == 'RGB':
+                        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                        batch = np.expand_dims(image_rgb, axis=0)
+                    else:
+                        batch = np.expand_dims(image, axis=0)
+                    prob = sess.run(model.prob, feed_dict={X: batch})
+                    '''END INFERENCE'''
+                    save_prediction_image(gal, image, prob[0])
+                    CC += 1
+        if FLAGS.db:
+            stream = picpac.ImageStream({'db': FLAGS.db, 'loop': False, 'channels': FLAGS.channels, 'colorspace': FLAGS.colorspace, 'threads': 1, 'shuffle': False,
+                                         'transforms': [{"type": "resize", "max_size": FLAGS.max_size},
+                                                        {"type": "clip", "round": FLAGS.clip_stride}]})
+            for meta, batch in stream:
+                    if CC > FLAGS.max:
+                        break
+                    print(meta.ids)
+                    image = batch[0]
+                    if FLAGS.channels == 3 and FLAGS.colorspace == 'RGB':
+                        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    prob = sess.run(model.prob, feed_dict={X: batch})
+                    '''END INFERENCE'''
+                    save_prediction_image(gal, image, prob[0])
+                    CC += 1
+        gal.flush()
     pass
 
 if __name__ == '__main__':
